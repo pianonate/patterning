17edc2c489de20414a9c7ff416b2dba4329f2bfc
Nate McCoy <you@example.com>
2023-05-12 00:24:32 -0400

    renamed

--- src/main/java/LifeFormats.java
+++ src/main/java/LifeFormats.java
@@ -1,8 +1,9 @@
 /**
  * you exist because there are more to come
  */
-public final class Formats {
+public final class LifeFormats {
 
+    // first of many parsers
     LifeForm parseRLE(String text) throws NotLifeException {
         return new RLEParser(text).getResult();
     }
