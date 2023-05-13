9ba53167ed1ba4929c226f1e474eb2b4b2464599
Nate McCoy <you@example.com>
2023-05-13 17:37:11 -0400

    added gradle files - for the people

--- gradlew.bat
+++ gradlew.bat
@@ -5,8 +5,8 @@
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
 @rem
-@rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
