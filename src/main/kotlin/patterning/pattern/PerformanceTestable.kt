19abc50b74fda6debcea65d0a15cce9e8abfa4e4
Nate McCoy <you@example.com>
2023-08-08 14:46:09 -0400

    capabilities interface - preparation for separating UX drawing from pattern drawing

---
+++ src/main/kotlin/patterning/pattern/PerformanceTestable.kt
@@ -0,0 +1,6 @@
+package patterning.pattern
+
+interface PerformanceTestable {
+    // currently we're just indicating that the pattern will support a
+    // running mode of Testing and that we will handle the test appropriately
+}
