777b096a0c3751983f9843006e72d637646e91e2
Nate McCoy <you@example.com>
2023-08-23 16:45:44 -0400

    clean up BoundingBox

--- src/main/kotlin/patterning/life/BoundingBox.kt
+++ src/main/kotlin/patterning/life/BoundingBox.kt
@@ -7,31 +7,9 @@ import patterning.util.roundToIntIfGreaterThanReference
 import processing.core.PApplet
 import processing.core.PGraphics
 
-class BoundingBox(private val bounds: Bounds, private val canvas: Canvas) {
+class BoundingBox(bounds: Bounds, private val canvas: Canvas) {
     
-    data class BoundingBoxSnapshot(
-        val leftBD: FlexibleDecimal,
-        val topBD: FlexibleDecimal,
-        val leftZoomed: FlexibleDecimal,
-        val offsetX: FlexibleDecimal,
-        val leftWithOffset: FlexibleDecimal,
-        val topZoomed: FlexibleDecimal,
-        val offsetY: FlexibleDecimal,
-        val topWithOffset: FlexibleDecimal,
-        val widthDecimal: FlexibleDecimal,
-        val heightDecimal: FlexibleDecimal,
-        val left: Float,
-        val top: Float,
-        val right: Float,
-        val bottom: Float,
-        val width: Float,
-        val height: Float,
-        val bounds: Bounds,
-        val halfHeight: FlexibleDecimal,
-        val startYDecimal: FlexibleDecimal,
-    )
-    
-    val mc = canvas.mc
+    private val mc = canvas.mc
     private val leftBD = bounds.left.toFlexibleDecimal().scale(mc)
     private val topBD = bounds.top.toFlexibleDecimal().scale(mc)
     
@@ -70,21 +48,11 @@ class BoundingBox(private val bounds: Bounds, private val canvas: Canvas) {
     // Calculate Lines
     private val horizontalLine: Line
         get() {
-            count++
             val startX = left
             
             // divide by half cell size because the draw algorithm ends up with cells at center of universe right now...
             val halfHeight = heightDecimal.divide(FlexibleDecimal.TWO, canvas.mc)
             val startYDecimal = topWithOffset + halfHeight
-            
-            
-            if (count % 2 == 0) {
-                secondSnapshot = createSnapshot(halfHeight, startYDecimal)
-                val discrepancies = compareSnapshots(firstSnapshot!!, secondSnapshot!!)
-                discrepancies.forEach(::println)
-            } else {
-                firstSnapshot = createSnapshot(halfHeight, startYDecimal)
-            }
             
             val startY = when {
                 startYDecimal < FlexibleDecimal.ZERO -> negativeOffScreen
@@ -120,92 +88,6 @@ class BoundingBox(private val bounds: Bounds, private val canvas: Canvas) {
             
             return Line(Point(startX, top), Point(startX, endY))
         }
-    
-    private fun compareSnapshots(snapshot1: BoundingBoxSnapshot, snapshot2: BoundingBoxSnapshot): List<String> {
-        val discrepancies = mutableListOf<String>()
-        
-        if (snapshot1.leftBD.multiply(
-                FlexibleDecimal.TWO,
-                canvas.mc
-            ).scale(mc) != snapshot2.leftBD
-        ) discrepancies.add("Discrepancy in leftBD: ${snapshot1.leftBD} vs ${snapshot2.leftBD}")
-        
-        if (snapshot1.topBD.multiply(
-                FlexibleDecimal.TWO,
-                canvas.mc
-            ).scale(mc) != snapshot2.topBD
-        ) discrepancies.add("Discrepancy in topBD: ${snapshot1.topBD} vs ${snapshot2.topBD}")
-        
-        if (snapshot1.leftZoomed.multiply(
-                FlexibleDecimal.TWO,
-                canvas.mc
-            ).scale(mc) != snapshot2.leftZoomed
-        ) discrepancies.add("Discrepancy in leftZoomed: ${snapshot1.leftZoomed} vs ${snapshot2.leftZoomed}")
-        
-        val leftDelta = (snapshot1.leftWithOffset + snapshot1.leftZoomed).scale(mc)
-        if (leftDelta != snapshot2.leftWithOffset) {
-            discrepancies.add("Discrepancy in leftWithOffset: ${snapshot1.leftWithOffset} + leftZoomed:${snapshot1.leftZoomed} = $leftDelta but is instead vs ${snapshot2.leftWithOffset} for a difference of: ${snapshot2.leftWithOffset - leftDelta}")
-            discrepancies.add("             zoomed + canvasOffset = leftWithOffset ${snapshot1.leftZoomed} + ${snapshot1.offsetX} = ${snapshot1.leftWithOffset} vs. ${snapshot2.leftZoomed} + ${snapshot2.offsetX} = ${snapshot2.leftWithOffset} ")
-        }
-        
-        if (snapshot1.topZoomed.multiply(
-                FlexibleDecimal.TWO,
-                canvas.mc
-            ).scale(mc) != snapshot2.topZoomed
-        ) discrepancies.add("Discrepancy in topZoomed: ${snapshot1.topZoomed} vs ${snapshot2.topZoomed}")
-        
-        val topDelta = (snapshot1.topWithOffset + snapshot1.topZoomed).scale(mc)
-        if (topDelta != snapshot2.topWithOffset) {
-            discrepancies.add("Discrepancy in topWithOffset: ${snapshot1.topWithOffset} + leftZoomed:${snapshot1.topZoomed} = $leftDelta but is instead vs ${snapshot2.topWithOffset} for a difference of: ${snapshot2.topWithOffset - topDelta}")
-            discrepancies.add("             zoomed + canvasOffset = topWithOffset ${snapshot1.topZoomed} + ${snapshot1.offsetY} = ${snapshot1.topWithOffset} vs. ${snapshot2.topZoomed} + ${snapshot2.offsetY} = ${snapshot2.topWithOffset} ")
-            
-        }
-        if (snapshot1.widthDecimal.multiply(
-                FlexibleDecimal.TWO,
-                canvas.mc
-            ).scale(mc) != snapshot2.widthDecimal
-        ) discrepancies.add("Discrepancy in widthDecimal: ${snapshot1.widthDecimal} vs ${snapshot2.widthDecimal}")
-        
-        if (snapshot1.heightDecimal.multiply(
-                FlexibleDecimal.TWO,
-                canvas.mc
-            ).scale(mc) != snapshot2.heightDecimal
-        ) discrepancies.add("Discrepancy in heightDecimal: ${snapshot1.heightDecimal} vs ${snapshot2.heightDecimal}")
-        
-        if (snapshot1.left != snapshot2.left) discrepancies.add("Discrepancy in left: ${snapshot1.left} vs ${snapshot2.left}")
-        if (snapshot1.top != snapshot2.top) discrepancies.add("Discrepancy in top: ${snapshot1.top} vs ${snapshot2.top}")
-        if (snapshot1.right != snapshot2.right) discrepancies.add("Discrepancy in right: ${snapshot1.right} vs ${snapshot2.right}")
-        if (snapshot1.bottom != snapshot2.bottom) discrepancies.add("Discrepancy in bottom: ${snapshot1.bottom} vs ${snapshot2.bottom}")
-        if (snapshot1.width != snapshot2.width) discrepancies.add("Discrepancy in width: ${snapshot1.width} vs ${snapshot2.width}")
-        if (snapshot1.height != snapshot2.height) discrepancies.add("Discrepancy in height: ${snapshot1.height} vs ${snapshot2.height}")
-        
-        return discrepancies
-    }
-    
-    
-    private fun createSnapshot(halfHeight: FlexibleDecimal, startYDecimal: FlexibleDecimal): BoundingBoxSnapshot {
-        return BoundingBoxSnapshot(
-            leftBD = leftBD,
-            topBD = topBD,
-            leftZoomed = leftZoomed,
-            offsetX = offsetX,
-            leftWithOffset = leftWithOffset,
-            topZoomed = topZoomed,
-            offsetY = offsetY,
-            topWithOffset = topWithOffset,
-            widthDecimal = widthDecimal,
-            heightDecimal = heightDecimal,
-            left = left,
-            top = top,
-            right = right,
-            bottom = bottom,
-            width = width,
-            height = height,
-            bounds = bounds,
-            halfHeight = halfHeight,
-            startYDecimal = startYDecimal,
-        )
-    }
     
     
     private fun drawCrossHair(graphics: PGraphics) {
@@ -275,10 +157,6 @@ class BoundingBox(private val bounds: Bounds, private val canvas: Canvas) {
     }
     
     companion object {
-        
-        var count = 0
-        var firstSnapshot: BoundingBoxSnapshot? = null
-        var secondSnapshot: BoundingBoxSnapshot? = null
         
         // we draw the box just a bit off screen so it won't be visible
         // but if the box is more than a pixel, we need to push it further offscreen
